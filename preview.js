989cf40a1cfed6fdae390c42c17d4791621a5429
Echonect <echonectwizard@gmail.com>
2023-01-20 14:34:12 +1000

    Playing with basic Javascript to display Gif.

---
+++ preview.js
@@ -0,0 +1,6 @@
+var name = "John Doe";
+alert(`Hi, ${name}. Want to see something cool?`);
+document.querySelector('html').innerHTML = `
+    <h1>Welcome to ${name}'s site<h1>
+    <img src="GrinchGif.webp">
+`;
